04fd750b2a67c96af0c03d6d630654252e05cb38
sainttobs <sainttobs@gmail.com>
2018-08-08 01:15:17 +0100

    fixed registration. It now works

--- registerscript.php
+++ registerscript.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
  require('config.php');
-if (isset($_POST['username']) and isset($_POST['password'])){
-$username = $_POST['username'];
+if (isset($_POST['name']) and isset($_POST['password'])){
+$name = $_POST['name'];
 $password =md5($_POST['password']);
 $email = $_POST['email'];
-$query = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
+$query = "SELECT * FROM `users` WHERE name='$name' and password='$password'";
  
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $count = mysqli_num_rows($result);
 if ($count == 0){
 // $_SESSION['username'] = $username;
-$query = "INSERT INTO `user`(`username`,`password`,`email`) VALUES('".$username."','".$password."','".$email."')";
+$query = "INSERT INTO `users`(`name`,`password`,`email`) VALUES('".$name."','".$password."','".$email."')";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 if($result){
     header('Content-type: application/json');  
-    $array = array('result2' => "Registered",'data'=>$username);
+    $array = array('result2' => "Registered",'data'=>$name);
     echo json_encode($array);
 }
 
